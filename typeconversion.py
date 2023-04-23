5cbe6591ffd08669579ca1a0771e7872b200ada2
Bhavesh Gupta <bhuvigupta50@gmail.com>
2023-04-23 18:05:36 +0530

    type conversion

---
+++ typeconversion.py
@@ -0,0 +1,11 @@
+#implicit is done by python 
+print(5+562.5)
+print(5+5j+5)
+
+
+#explicit is not done by python
+#a=input("enter ")
+#b=int(input("enter "))#here we explicitly type integer
+#print(a+b)
+
+print(list('bhavesh'))
