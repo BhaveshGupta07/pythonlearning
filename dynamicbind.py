3345eba027ae2c81e7fb71e29b374466c0fc7140
Bhavesh Gupta <bhuvigupta50@gmail.com>
2023-04-23 17:50:23 +0530

    declare a variable

---
+++ dynamicbind.py
@@ -0,0 +1,17 @@
+name='bhavesh'
+print(name)
+
+x=545545
+print(x)
+
+help=False
+print(help)
+
+c=0
+print(c)
+
+a,b,c,d=5,4,8,5
+print(a,b,c,d)
+
+
+#in python we don't need to define the variables datatype we can use our variable anytime and change its value
