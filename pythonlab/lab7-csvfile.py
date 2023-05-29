42159310fe0a2a2237edba02e1f275367592d051
Bhavesh Gupta <bhuvigupta50@gmail.com>
2023-05-29 22:25:24 +0530

    changes

--- pythonlab/lab7-csvfile.py
+++ pythonlab/lab7-csvfile.py
@@ -1,11 +1,9 @@
-#import csv
-#fileApath = input("Enter the input file path: ")
-##fileBpath = input("Enter the output file path: ")
-#with open(fileApath, 'r') as fileA:
-    #with open(fileBpath, 'w', newline='') as fileB:
-       # reader = csv.reader(fileA)
-        #writer = csv.writer(fileB)
-       # writer.writerows(reader)
-print("Enter the input file path: student.csv")
-print("Enter the output file path: student2.csv")
+import csv
+fileApath = input("Enter the input file path: ")
+fileBpath = input("Enter the output file path: ")
+with open(fileApath, 'r') as fileA:
+    with open(fileBpath, 'w', newline='') as fileB:
+        reader = csv.reader(fileA)
+        writer = csv.writer(fileB)
+        writer.writerows(reader)
 print("Completed copying.")
